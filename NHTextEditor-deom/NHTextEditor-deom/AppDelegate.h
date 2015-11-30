5e53d071e3ffc870f95c8de113b85379555333e2
Wilson Yuan <yuansc@nihao.com>
2015-11-30 11:14:33 +0800

    Initial

---
+++ NHTextEditor-deom/NHTextEditor-deom/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NHTextEditor-deom
+//
+//  Created by Wilson Yuan on 15/11/30.
+//  Copyright © 2015年 Wilson-Yuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NHTextEditor-deom/NHTextEditor-deomTests/NHTextEditor_deomTests.m
@@ -0,0 +1,39 @@
+//
+//  NHTextEditor_deomTests.m
+//  NHTextEditor-deomTests
+//
+//  Created by Wilson Yuan on 15/11/30.
+//  Copyright © 2015年 Wilson-Yuan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NHTextEditor_deomTests : XCTestCase
+
+@end
+
+@implementation NHTextEditor_deomTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
